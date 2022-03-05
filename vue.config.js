2e3b9136ed03fd75d8ff690d1810fdfbffbe4496
vaibhavk-uk <vaibhavk.uk@gmail.com>
2022-03-05 15:14:26 +0530

    Sudhansh Dev

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,32 @@
 module.exports = {
-    pluginOptions: {
-      electronBuilder: {
-        nodeIntegration: true,
-        builderOptions: {
-          publish: ['github']
+  pluginOptions: {
+    electronBuilder: {
+      builderOptions: {
+        "appId": "com.electron.Wolverine-Remastered-2022",
+        "productName": "Wolverine-Remastered-2022",
+        "publish": [
+          {
+            "provider": "github",
+            "owner": "2727-ask",
+            "repo": "ele-updator"
+          }
+        ],
+        "win": {
+          "publish": [
+            "github"
+          ],
+          "target": [
+            {
+              "target": "nsis",
+              "arch": [
+                "x64",
+                "ia32"
+              ]
+            }
+          ]
         }
+
       }
     }
   }
+}
