2e7bcc79b88ad7aaa67818885e1d5adfad10ff5b
ashutosh-indianpix <99986604+ashutosh-indianpix@users.noreply.github.com>
2022-03-19 18:22:05 +0530

    First Commit After A Problem , Unfortunate Commit Was Done By Me

---
+++ src/components/Tabs.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="uk-button-group uk-flex uk-flex-center">
+    <router-link to="/newrecord" class="uk-button uk-button-primary btn">Record</router-link>
+    <router-link to="/doctors" class="uk-button uk-button-primary btn">Doctors</router-link>
+    <router-link to="/facilities" class="uk-button uk-button-primary btn">Facilities</router-link>
+    <router-link to="/payments" class="uk-button uk-button-primary btn">Payments</router-link>
+    <a to="#" @click="refreshRequest" class="uk-button uk-button-primary btn">Refresh</a>
+  </div>
+</template>
+
+<script>
+  export default{
+    methods:{
+      makeDoctorsGetReq() {
+      console.log("Triggered");
+      this.$store.dispatch({
+        type: "doctors/get_doctors",
+      });
+    },
+      makefacilitiesGetReq() {
+      console.log(this.resultQuery);
+        console.log("Triggered");
+        this.$store.dispatch({
+          type: "facilities/get_facilities",
+        });
+    },
+    refreshRequest(){
+      this.makeDoctorsGetReq();
+      this.makefacilitiesGetReq();
+    }
+
+    }
+  }
+</script>
+
+<style scoped>
+.uk-button-group{
+  overflow:scroll;
+}
+.btn{
+    margin:20px;
+}
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,54 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import Settings from '../screens/Settings.vue';
+
+
+const routes = [
+  { path: '/', redirect: '/newrecord' },
+  {
+    path: '/newrecord',
+    name: 'NewRecord',
+    component: () => import('../screens/NewRecord.vue')
+  },
+  {
+    path: '/doctors',
+    name: 'Doctors',
+    component: () => import('../screens/Doctors.vue')
+  },
+  {
+    path: '/facilities',
+    name: 'Facilities',
+    component: () => import('../screens/Facilities.vue')
+  },
+  {
+    path: '/payments',
+    name: 'Payments',
+    component: () => import('../screens/Payments.vue')
+  },
+  {
+    path: '/detailed_payments/',
+    name: 'Payment_Details',
+    component: () => import('../screens/PaymentDetails.vue')
+  },
+  {
+    path: '/your_profile/',
+    name: 'Your_Profile',
+    component: () => import('../screens/YourProfile.vue')
+  },
+  {
+    path: '/print/',
+    name: 'Print',
+    component: () => import('../screens/Print.vue')
+  },
+  {
+    path: '/settings',
+    name: 'Settings',
+    component: Settings
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router
